92704ee0cd12fbd3afbd5c141d16c971c679bf99
yuhaneur <yuhaneur@naver.com>
2023-07-15 02:16:33 +0900

    common

--- 0714오후/script/list.js
+++ 0714오후/script/list.js
@@ -28,33 +28,34 @@ $(function(){//페이지가 모두 로드(화면에 표시할꺼) 되면 실행
 
     
 function make(){
-// 반복문을 이용해서 table 태그를 만들고
-// 테이블안에 내용을 출력한다.
-
-const list = ["이순신","김유신","장보고","강해민","강미정",
-"김선경","김재호","이채영","이재원","여상현","이예진"];
-
-const addr =["대흥동","선화동","은행동","중리동","둔산1동",
-"둔산2동","탄방동","태평동","문화동","갈마동","소재동","삼성동"];
-
-var out="<h3>투게더 명단 ("+list.lengt+"명)</h3>";
-
-out +="<table class='1ist_box'>";
-}
-for(var i=0;i<list.length;i++){//list배열의 이름만큼 반복}
-    out +="<tr>";
-    for(var k=1; k<=3; k++){
-        out += "<td class='name'><span>"+list[i]+"</span> </td>";
-        out +="<td class='age'>"+(Math.floor(Math.random()*16)+20 )+"</td>";
-        var n=Math.floor(Math.random()*addr.length);
-        out +="<td class='addr'>"+addr[n]+"</td>";
-
-    out+= "</tr>";
-}
-out +="</table>";
-
-$("#section").html(out);
-}
+    // 반복문을 이용해서 table 태그를 만들고
+    //  테이블안에  내용을 출력한다.
+    
+    const list = ["이순신","김유신","장보고","강해민","강미정","김선경","김재호",
+    "이채영","이재원","여상현","이예진"];
+    
+    const addr=["대흥동","선화동","은행동","중리동","둔산1동","둔산2동",
+    "탄방동","태평동","문화동","갈마동","소재동","삼성동"];
+    
+    var out="<h3>투게더 명단 ("+list.length+"명)</h3>";
+    
+    out += "<table class='list_box'>";
+    
+    for( var i=0; i<list.length; i++){// list배열의 이름 만큼 반복
+        out += "<tr>";
+        
+        out += "<td class='name'> <span>"+list[i]+"</span> </td>";
+        out += "<td class='age'>"+(Math.floor(Math.random()*16 )+20 )+"</td>";
+        var n=Math.floor(Math.random()*addr.length ) ;
+        out += "<td class='addr'>"+addr[n]+"</td>";
+    
+        out += "</tr>";
+    }
+    
+    out += "</table>";
+    
+    $("#section").html(out);
+    }
 /*
     기본 슬라이드 구현
     1.하나씩 나타내기
